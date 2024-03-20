1d48f9fde0c53160830d7df3bffa9becb72ac4db
Salman-sajib <sfsajib@gmail.com>
2024-03-20 21:44:25 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import PathTwitter from './components/PathTwitter';
+
+function App() {
+  return (
+    <div className='app'>
+      <PathTwitter />
+    </div>
+  );
+}
+
+export default App;
